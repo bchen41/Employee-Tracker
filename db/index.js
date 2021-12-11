c4c100982e8f46cc517a49b9b17dfe242a8113da
bchen41 <bettychen41@outlook.com>
2021-12-11 15:18:39 -0500

    created function selectAllEmployees

--- db/index.js
+++ db/index.js
@@ -21,10 +21,24 @@ class DB {
   selectAllRoles() {
     return this.connection.query(
       `
-    SELECT role.id, role.title, role.salary, department.name AS department
-    FROM role
-    LEFT JOIN department ON role.department_id = department.id
-    `
+      SELECT role.id, role.title, role.salary, department.name AS department
+      FROM role
+      LEFT JOIN department ON role.department_id = department.id
+      `
+    );
+  }
+
+  // Select all employees, join with roles and departments to display their roles, salaries, departments, and managers
+  selectAllEmployees() {
+    return this.connection.query(
+      `
+      SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary,
+      CONCAT (manager.first_name, " ", manager.last_name) AS manager
+      FROM employee 
+      LEFT JOIN role ON employee.role_id = role.id 
+      LEFT JOIN department ON role.department_id = department.id 
+      LEFT JOIN employee manager ON manager.id = employee.manager_id
+      `
     );
   }
 }
